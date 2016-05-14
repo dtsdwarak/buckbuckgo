bd28bcdcf453e8c47bf01584e7bd1a77eb702923
Dwaraka Nath <dtsdwarak@hotmail.com>
2016-05-14 20:03:17 +0530

    Implemented Ajax Result fetch. Yay. :satisfied: :sunglasses:

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,4 +8,19 @@ class SearchController < ApplicationController
       @search_results = DuckDuckGo.new.zeroclickinfo(@search_param)
       @hash = @search_results.related_topics
   end
+
+  def ajax
+    require "duck_duck_go"
+    require "json"
+    search_param = params[:search_text]
+    search_results = DuckDuckGo.new.zeroclickinfo(search_param)
+    hash = search_results.related_topics
+    arr = Array.new
+    hash.each do |key, array|
+        array.each do |x|
+            arr << "<a href='https://" + x.first_url.hostname + x.first_url.request_uri + "'>" + x.result[/(>\s*(.*?)\s*<)/,2] + "</a>"
+        end
+    end
+    render :json => arr.first(7)
+  end
 end
